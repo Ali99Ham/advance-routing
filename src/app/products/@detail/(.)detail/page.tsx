05f8a2f26c90e48177ea87469f052364ae4b4aa5
unknown <ali.hamdan@wonderdays.com>
2025-05-02 22:00:18 +0400

    close modal intercept

--- src/app/products/@detail/(.)detail/page.tsx
+++ src/app/products/@detail/(.)detail/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const page = () => {
-  return <div>Products Interceted Details Page</div>;
+  return (
+    <div>
+      Products Interceted Details Page
+      <p>To close the modal , just need to go back one step in the url</p>
+    </div>
+  );
 };
 
 export default page;
